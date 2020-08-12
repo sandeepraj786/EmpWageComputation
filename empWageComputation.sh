17041869a13ae8df56130cd63d1e1e7f4f50364d
sandeep786 <sadeeprajsingh05@gmail.com>
2020-08-12 19:36:51 +0530

    added wage for month to employee wage computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,30 @@
 #!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
+#!/bin/bash -x
 isPartTime=1
 isFullTime=2
+
 empRatePerHrs=20
+noOfWorkingDays=20
+totalSal=0
 
-empCheck=$((RANDOM%3))
+for ((day=1;day<$noOfWorkingDays;day++))
+do
+        empCheck=$((RANDOM%3))
 
-case $empCheck in
-        $isPartTime)
+        case $empCheck in
+                $isPartTime)
                         empHrs=4 ;;
-        $isFullTime)
+                $isFullTime)
                         empHrs=8 ;;
-        	  *)
+                *)
                         empHrs=0 ;;
-esac
+        esac
 
-sal=$(($empHrs*empRatePerHrs))
-echo $sal
+        sal=$(($empHrs*empRatePerHrs))
+        totalSal=$(($totalSal+$sal))
+done
+echo $totalSal
+
 
