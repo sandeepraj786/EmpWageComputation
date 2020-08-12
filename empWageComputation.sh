dc057fff20b3f791723ba58cd6ebacbb92083652
sandeep786 <sadeeprajsingh05@gmail.com>
2020-08-12 18:25:39 +0530

    added daily wage to employee wage computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
-#usecase being solved showing employee is present or not.
-
 isPresent=1
+wagePerHours=20
+
 attendanceCheck=$((RANDOM%2))
 if (( attendanceCheck == isPresent ))
 then
 	echo Employee is Present
+	empHrs=8
+	totalWage=$(($wagePerHours*$empHrs))
 else
 	echo Employee is Absent
 fi
