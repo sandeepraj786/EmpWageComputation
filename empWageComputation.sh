978e62e62558bbe8d7cb56abbcd585ef05ba9900
sandeep786 <sadeeprajsingh05@gmail.com>
2020-08-12 19:20:43 +0530

    added case statement to employee wage computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -x
 echo Welcome To Employee Wage problem.
 
-isPresent=1
-attendanceCheck=$((RANDOM%2))
-randomValue=$((RANDOM%2))
-fullTime=1
-wagePerHour=20
-if (( attendanceCheck == isPresent ))
-then
-	if (( randomValue == fullTime ))
-	then
-		totalWage=$((wagePerHour*8))
-		echo "Employee is present and his daily wage is $totalWage"
-	else
-		partimeWage=$((wagePerHour*4))
-		echo "Employee is present and he works part time and his daily wage is $partimeWage"
-	fi
-else
-	echo Employee is Absent
-fi
+isPartTime=1
+isFullTime=2
+empRatePerHrs=20
+
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+        $isPartTime)
+                        empHrs=4 ;;
+        $isFullTime)
+                        empHrs=8 ;;
+        	  *)
+                        empHrs=0 ;;
+esac
+
+sal=$(($empHrs*empRatePerHrs))
+echo $sal
+
